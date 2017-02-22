7c4c2f518f9bcf884044aa6e5c78846f1da5787c
Martin Heidegger <martin.heidegger@gmail.com>
2017-02-22 14:26:17 +0900

    Docs and general upgrade

--- Dockerfile
+++ Dockerfile
@@ -2,21 +2,24 @@ FROM ruby:2.3.0-slim@sha256:8f3ae4ed9790a4b2c41a891deef876a204690b8d7c6bb8cf9ec0
 
 WORKDIR /usr/src/app
 
+# NOTE: libxml2-dev libxslt-dev for nokogiri (actionview 4.2 <- rails-html-sanitizer)
+# NOTE: libffi-dev for ffi. see https://github.com/ffi/ffi/issues/485#issuecomment-191382158
+# NOTE: gcc, g++ and libc-dev for json gem
+# NOTE: linux-headers for raindrops gem
+# NOTE: bash for ci
 RUN mkdir -p /usr/src/app \
     && apt-get update \
-    && apt-get install -y make python2.7 gcc g++ man curl git \
+    && apt-get install -y make python2.7 gcc g++ man curl git linux-headers-amd64 libffi-dev libxml2-dev libxslt-dev  \
     && ln -s /usr/bin/python2.7 /usr/bin/python \
     && curl -sL https://raw.githubusercontent.com/martinheidegger/install-node/master/install_node.sh | \
        NODE_VERSION="v5.1.0" \
-       YARN_VERSION="v0.19.1" \
+       YARN_VERSION="v0.20.3" \
        bash \
     && rm -rf /var/cache/apk/* /var/lib/apt/lists/* /usr/share/doc /usr/share/perl* /usr/share/man || true
 
 ADD entrypoint.sh /
-RUN    echo "export BUNDLE_PATH=/bundle"  >> /etc/profile \
-    && echo "export PATH=\$PATH:/tmp/bin" >> /etc/profile
 
-ENTRYPOINT ["/entrypoint.sh"]
+ENTRYPOINT ["/bin/bash", "-l", "/entrypoint.sh"]
 
 ONBUILD ADD package.json yarn.lock ./
 ONBUILD RUN yarn install --no-emoji --ignore-optional --strict-semver --network-concurrency=15 --no-cache
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,6 @@
 
 trap exit INT TERM EXIT;
 
-
 log () {
   CNT=${#1}
   node -p "'___' + (new Array($CNT)).join('_')"
@@ -15,26 +14,78 @@ run () {
   $1
 }
 
+TMP=/usr/src/app/tmp
+mkdir -p ${TMP}/out
+
+read_new_lines () {
+  if [ ! -f $1 ]; then
+    return
+  fi
+  readarray outlines <$1
+  LINE_NR=0
+  for line in "${outlines[@]}"; do
+    ((LINE_NR=LINE_NR+1))
+    if [ ${LINE_NR} -gt ${LINES_READ} ]; then
+      export LINES_READ=${LINE_NR}
+      printf "${line}"
+    fi
+  done
+}
+
+parallel_by_line () {
+  CNT=0
+  readarray lines
+  for line in "${lines[@]}"; do
+    (echo "$line" | /bin/bash) >${TMP}/out/parallel_${CNT}.out 2>${TMP}/out/parallel_${CNT}.err &
+    PIDS="$PIDS $!"
+    ((CNT=CNT+1))
+  done
+
+  CNT=0
+  ERROR=false
+  for pid in $PIDS; do
+    if [ ${ERROR} != "true" ]; then
+      export LINES_READ=0
+      while [ -e /proc/$pid ]; do
+        read_new_lines ${TMP}/out/parallel_${CNT}.out
+      done
+      read_new_lines ${TMP}/out/parallel_${CNT}.out
+      cat ${TMP}/out/parallel_${CNT}.err >&2
+      wait $pid || ERROR=true
+    else
+      kill $pid >/dev/null 2>&1
+    fi
+    rm ${TMP}/out/parallel_${CNT}.*
+    ((CNT=CNT+1))
+  done
+  if [ ${ERROR} == "true" ]; then
+    return 1
+  fi
+}
+
+rm ${TMP}/pids/server.pid 2>/dev/null
+
 log "$0"
-echo "» pwd:         `pwd`"
-echo "» PATH:        ${PATH}"
-echo "» BUNDLE_PATH: ${BUNDLE_PATH}"
-echo "» bundler:     `bundle --version`"
-echo "» gem:         `gem --version`"
-echo "» ruby:        `ruby --version`"
-echo "» node:        `node --version`"
-echo "» yarn:        `yarn --version`"
-echo "» python:      $(python -c 'from platform import python_version; print python_version()')"
-
-# some restarts don't kill the server
-rm /usr/src/app/tmp/pids/server.pid 2>/dev/null
-
-log "bundle check || bundle install" 
-bundle check || bundle install || exit 1
-
-run "yarn install --no-emoji --ignore-optional --strict-semver --network-concurrency=15" || exit 1
-
-CNT=${#@}
+(cat <<TASKS
+  echo "▶︎ pwd:         `pwd`"
+  echo "▶︎ PATH:        ${PATH}"
+  echo "▶︎ BUNDLE_PATH: ${BUNDLE_PATH}"
+  echo "▶︎ bundler:     `bundle --version`"
+  echo "▶︎ gem:         `gem --version`"
+  echo "▶︎ ruby:        `ruby --version`"
+  echo "▶︎ node:        `node --version`"
+  echo "▶︎ yarn:        `yarn --version`"
+  echo "▶︎ python:      $(python -c 'from platform import python_version; print python_version()')"
+  echo ""
+  echo "⦿ Installing node dependencies"
+  echo ""
+  yarn install --no-emoji --ignore-optional --strict-semver --network-concurrency=15
+  echo ""
+  echo "⦿ Installing gems"
+  echo ""
+  bundle check || bundle install
+  echo ""
+TASKS
+) | parallel_by_line || exit 1
 
 run "$@" || exit 1
-
